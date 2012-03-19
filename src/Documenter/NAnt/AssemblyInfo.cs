f19a2c966ae32cc1f36ee6e5783bfa4f07873d21
Tom Parker <palfrey@tevp.net>
2012-03-19 23:36:25 +0000

    Pull in the NAnt documenter class

---
+++ src/Documenter/NAnt/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System;
+using System.Reflection;
+
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyTitle("NDoc NAnt Documenter")]
+[assembly: AssemblyDescription("NAnt documenter for the NDoc code documentation generator.")]
